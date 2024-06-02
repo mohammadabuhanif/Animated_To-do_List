ae8405fde308a0fedfe6e85e1b27d2ac3da0a926
Mohammad Abu Hanif <88657042+mohammadabuhanif@users.noreply.github.com>
2024-06-02 07:14:34 +0600

    Update script.js
    Added Drag and Drop priority for lists and Google Drive backup to connect in another device with same data.

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
-// add task onclick
 function addTask() {
     if (inputBox.value === '') {
         alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7"
     }
-    else {
+
+}
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+// Add task onclick
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
@@ -17,17 +30,6 @@ function addTask() {
     saveData();
 }
 
-listContainer.addEventListener("click", function (e) {
-    if (e.target.tagName === "LI") {
-        e.target.classList.toggle("checked");
-        saveData()
-    }
-    else if (e.target.tagName === "SPAN") {
-        e.target.parentElement.remove();
-        saveData()
-    }
-}, false);
-
 function saveData() {
     localStorage.setItem("data", listContainer.innerHTML);
 }
@@ -37,7 +39,7 @@ function showTask() {
 }
 
 showTask();
-
+// Typed text
 let input = document.querySelector(".input");
 let slider_box = document.querySelector(".slider_box");
 let slider = document.querySelector(".slider");
@@ -45,7 +47,7 @@ let placeholder = document.querySelector(".placeholder");
 
 let list = ["Fitness Routine", "Groceries", "Events", "Pomodoro"];
 let i = 0;
-text_animation()
+text_animation();
 let intervals = "";
 setintervals();
 
@@ -73,7 +75,6 @@ function setintervals() {
     }, 2500);
 }
 
-
 function text_animation() {
     i++;
     slider.innerHTML = list[i - 1];
@@ -87,3 +88,95 @@ function text_animation() {
         i = 0;
     }
 }
+
+function authenticateWithGoogle() {
+    window.location.href = `${googleAuthUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&response_type=code`;
+}
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        li.setAttribute("draggable", "true");
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+        li.addEventListener("dragstart", dragStart);
+        li.addEventListener("dragover", dragOver);
+        li.addEventListener("drop", drop);
+        li.addEventListener("dragend", dragEnd);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+// Drag and Drop for priority
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+    document.querySelectorAll('li').forEach(item => {
+        item.setAttribute("draggable", "true");
+        item.addEventListener("dragstart", dragStart);
+        item.addEventListener("dragover", dragOver);
+        item.addEventListener("drop", drop);
+        item.addEventListener("dragend", dragEnd);
+        saveData()
+    });
+}
+
+showTask();
+
+function dragStart(event) {
+    event.target.classList.add('dragging');
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.setData('text/html', event.target.innerHTML);
+    saveData()
+}
+
+function dragOver(event) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
+    saveData()
+}
+
+function drop(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    const draggingElement = document.querySelector('.dragging');
+    const targetElement = event.target.closest('li');
+
+    if (draggingElement && targetElement && draggingElement !== targetElement) {
+        const targetIndex = [...targetElement.parentNode.children].indexOf(targetElement);
+        const draggingIndex = [...draggingElement.parentNode.children].indexOf(draggingElement);
+
+        if (draggingIndex < targetIndex) {
+            targetElement.parentNode.insertBefore(draggingElement, targetElement.nextSibling);
+        } else {
+            targetElement.parentNode.insertBefore(draggingElement, targetElement);
+        }
+
+        saveData();
+    }
+}
+
+function dragEnd(event) {
+    event.target.classList.remove('dragging');
+    saveData()
+}
+
+
